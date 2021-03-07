01ef7831963118d1164dc3670e2c5d8d685ea1e6
z5257107 <z5257107@cse.unsw.edu.au>
2021-03-07 19:09:22 +1100

    date logic

---
+++ src/outbreak_time.py
@@ -0,0 +1,39 @@
+import json
+from pymongo import MongoClient
+# pprint library is used to make the output look more pretty
+from pprint import pprint
+# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+import datetime
+client = MongoClient(
+    "mongodb+srv://user:iBMu1UIQhIzoW8Qn@cluster0.uq4ht.mongodb.net/outbreak_articles?retryWrites=true&w=majority")
+db = client.outbreak_articles
+col = db.outbreak_details
+
+date_matches = []
+
+# this function works assuming the date format is always in "Month xx, year" format
+
+# def date(dtime):
+
+def time(dtime):
+
+    year = dtime.strftime("%Y")
+    month = dtime.strftime("%B")
+    day = dtime.strftime("%d")
+
+    for doc in col.find({"date": querySt}):
+        # print(doc['title'].strip())
+        # print(doc['location'].strip())
+        # print(doc['date'].strip())
+
+        date_match = {
+            'title': doc['title'].strip(),
+            # 'location': doc['location'].strip(),
+            'date': doc['date'].strip()
+        }
+
+        date_match_copy = date_match.copy()
+
+        date_matches.append(date_match_copy)
+
+    return date_matches
