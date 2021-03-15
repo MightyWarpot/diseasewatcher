bd9b297c745cafb6b99f12538d765128f49ba6a2
Frank Su <frank.su@student.unsw.edu.au>
2021-03-15 20:28:45 +1100

    More tests

--- Phase1/TestScripts/outbreak_region_test.py
+++ Phase1/TestScripts/outbreak_region_test.py
@@ -8,7 +8,24 @@ db = client.outbreak_articles
 col = db.outbreak_details
 
 
-def test_outbreak_location_basecase():
+def test_outbreak_location_Asia():
     res = region_filter('Asia', col)
     for entry in res:
         assert entry['region'] == 'Asia'
+
+
+def test_outbreak_location_Europe():
+    res = region_filter('Europe', col)
+    for entry in res:
+        assert entry['region'] == 'Europe'
+
+
+def test_outbreak_location_Africa():
+    res = region_filter('Africa', col)
+    for entry in res:
+        assert entry['region'] == 'Africa'
+
+
+def test_outbreak_location_noinput():
+    res = region_filter('', col)
+    assert res == []
