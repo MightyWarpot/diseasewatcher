3e7ea38f40c2741b3ba0fdc9c87cbc8d760a79f1
Josh <z5115700@ad.unsw.edu.au>
2021-03-30 20:53:00 +1100

    link to users request is in console now... will implement result display on front-end soon

--- Phase2/disease-watcher-site/src/pages/Outbreaks.js
+++ Phase2/disease-watcher-site/src/pages/Outbreaks.js
@@ -1,38 +1,67 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Axios from 'axios'
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 
+
 export default function Outbreaks() {
-
-    const API = Axios.create({
-        baseURL: 'https://cors-anywhere.herokuapp.com/disease-watcher.herokuapp.com/',
-        headers: {
-            'content-type': 'application/json'
-        }
-    })
-
-    API.get('/outbreak/?location=China&start date=11/01/1991')
-        .then((response) => {
-            console.log(response.data)
-        })
-
+    //grabbing styles
     const styles = useStyles()
+
+    // API functions have been commented out because API proxy doesn't allow too many requests uncomment if you need to check result with backend
+    // const API = Axios.create({
+    //     baseURL: 'https://cors-anywhere.herokuapp.com/disease-watcher.herokuapp.com/',
+    //     headers: {
+    //         'content-type': 'application/json'
+    //     }
+    // })
+
+    // API.get('/outbreak/?location=China&start date=11/01/1991')
+    //     .then((response) => {
+    //         console.log(response.data)
+    //     })
+
+    
+
+    //functions and variables for interactions
+    
+    // const [location, setLocation] = useState('');
+    // const [disease, setDisease] = useState('');
+    // const [continent, setContinent] = useState('');
+    // const [indexStart, setIndexStart] = useState('');
+    // const [indexEnd, setIndexEnd] = useState('');
+    // const [reportAfter, setReportAfter] = useState('');
+    // const [reportBefore, SetRepBefore] = useState('');
+
+    const [state, setState] = useState('')
+    
+    const handleTextChange = e =>{
+        const { target: {value, id } } = e
+        setState(prev  =>({
+            ...prev,
+            [id] : value
+        }))
+    }
+    console.log(state)
+
+    //Example of url that i need to request http://disease-watcher.herokuapp.com/outbreak/?location=China&disease=H7N9&start date=12/02/1998&end date=22/02/2040&region=Asia&start_index=0&end_index=10
+
+    console.log(`http://disease-watcher.herokuapp.com/outbreak/?location=${state.location}&disease=${state.disease}&start date=${state.reportAfter}&end date=${state.reportBefore}&region=${state.region}&start_index=${state.indexStart}&end_index=${state.indexEnd}`)
 
 
     return (
         <div>
-            <h2 className> Find Disease Information </h2>
+            <h2 className={styles.header}> Find Disease Information </h2>
                 <div className={styles.container}>
                 <form className={styles.form} noValidate autoComplete="off">
                     <div>
-                        <TextField id="location" label="Location" type="search" />
-                        <TextField id="disease" label="Type of Disease" type="search" />
-                        <TextField id="reportAfter" label="Outbreak Reported After (DD/MM/YYYY)" type="search" />
-                        <TextField id="reportBefore" label="Oubtreak Reported Before (DD/MM/YYYY)" type="search" />
-                        <TextField id="continent" label="Continent" type="search" />
-                        <TextField id="indexStart" label="Article Start Index" type="search" />
-                        <TextField id="indexEnd" label="Article End Index" type="search" />
+                        <TextField id="location" label="Country" type="search" onChange={handleTextChange} />
+                        <TextField id="disease" label="Type of Disease" type="search" onChange={handleTextChange} />
+                        <TextField id="region" label="Continent" type="search" onChange={handleTextChange}/>
+                        <TextField id="indexStart" label="Article Start Index" type="search" onChange={handleTextChange} />
+                        <TextField id="indexEnd" label="Article End Index" type="search" onChange={handleTextChange} />
+                        <TextField id="reportAfter" label="Outbreak Reported After" type="search" helperText="(DD/MM/YYYY)" onChange={handleTextChange} />
+                        <TextField id="reportBefore" label="Oubtreak Reported Before" type="search" helperText="(DD/MM/YYYY)"  onChange={handleTextChange}/>
                     </div>
                     <div>
                     </div>
@@ -53,6 +82,9 @@ const useStyles = makeStyles((theme) => ({
     },
     container: {
         textAlign: 'center'
+    },
+    header: {
+        marginLeft: '5%'
     }
 
   }));
