fa32a8344eb8781cad9278f268735dae19ba1ff4
Josh <z5115700@ad.unsw.edu.au>
2021-04-06 13:07:26 +1000

    added outbreak info to the top along with images & date

--- Phase2/disease-watcher-site/src/pages/Home.js
+++ Phase2/disease-watcher-site/src/pages/Home.js
@@ -12,7 +12,54 @@ export default function Home() {
 
     return (
         <div >
-            
+
+            <Grid container>
+                <Grid item xs={12}>
+                    <h1 className={styles.text}>Current Outbreaks</h1>
+                    <Grid container justify="center">
+                        <Grid key={1} item>
+                        <Paper elevation={3} className={styles.paper}>
+                            <span className={styles.dateFirst}>March 2, 2021</span>
+                            <h2 className={styles.text}>Norovirus </h2>
+                            <p className={styles.textInner}>The Taiwan CDC is reporting the number of visits to hospitals for diarrhea has recently increased, and the pathogens detected in cluster incidents are mainly norovirus, and most of them occur in the catering and hotel industry.</p>
+                            <img
+                                className={styles.img}
+                                src="http://outbreaknewstoday.com/wp-content/uploads/2019/01/21348_lores.jpg"
+                                alt="new"
+                                />
+                        </Paper>
+                        </Grid>
+                        <Grid key={2} item>
+                        <Paper elevation={1} className={styles.paper}>
+                            <span className={styles.date}>November 21, 2020</span>
+                            <h2 className={styles.text}>Influenza</h2>
+                            <p className={styles.textInner}>Federal and state authorities say a case of low pathogenic avian influenza has been detected in a commercial poultry flock in western Kentucky.</p>
+                            <img
+                                className={styles.img}
+                                src="http://outbreaknewstoday.com/wp-content/uploads/2017/03/urica-1251980_640.jpg"
+                                alt="new"
+                                />
+                        </Paper>
+
+                        </Grid>
+                        <Grid key={3} item>
+                        <Paper elevation={1} className={styles.paper}>
+                            <span className={styles.date}>December 15, 2020</span>
+                            <h2 className={styles.text}>Polio</h2>
+                            <p className={styles.textInner}>Polio outbreaks have been reported in the following Asian countries: Afghanistan, Burma (Myanmar), China, Malaysia, Pakistan, The Philippines, Tajikistan and Yemen. </p>
+                            <img
+                                className={styles.img}
+                                src="http://outbreaknewstoday.com/wp-content/uploads/2021/02/Capture-2.png"
+                                alt="new"
+                                />
+                        </Paper>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+
+            <br/>
+
             <div className={styles.banner}>
                 <Chart> </Chart>
                 {/* <img className={styles.center} src={Map}/> */}
@@ -26,36 +73,6 @@ export default function Home() {
             {/* <div className={styles.pageDisplay}>
                 
             </div> */}
-
-            <Grid container>
-                <Grid item xs={12}>
-                    <h2 className={styles.text}>Current Outbreaks</h2>
-                    <Grid container justify="center">
-                        <Grid key={1} item>
-                        <Paper elevation={0} className={styles.paper}>
-                            <p className={styles.text}>Measles</p>
-                            <p className={styles.textInner}>There is a current and ongoing outbreak of measles in New Zealand, with a high proportion of cases reported from the Auckland region. The New Zealand Ministry of Health is prioritising vaccination of at-risk groups including children under five years of age, people aged 15 – 29 years, Pacific peoples within those groups, children under 15 years of age who have not had a single dose of the vaccine, and babies aged 6 months and older who reside in Auckland.</p>
-                        </Paper>
-                        </Grid>
-                        <Grid key={2} item>
-                        <Paper elevation={0} className={styles.paper}>
-                            <p className={styles.text}>Influenza</p>
-                            <p className={styles.textInner}>In the period March 18-31, Poland’s chief veterinary office confirmed a total of 42 outbreaks of highly pathogenic avian influenza HPAI in poultry flocks. All but two of the outbreaks were in commercial flocks, and the total number of birds involved in these outbreaks were almost 1.07 million. At each location, the presence of the H5N8 virus variant was confirmed. One area has become a particular hot spot for HPAI cases, namely Kalisz County in Greater Poland (Wielkopolskie). Over this period, the first HPAI cases have been confirmed in the province of Silesia (Slaskie), and other outbreaks have hit premises in Mazovia and Lesser Poland (Malopolskie). While Silesia and Lesser Poland are neighboring provinces in the south of the country, Greater Poland and Mazovia are in central Poland.
-                            These latest cases bring to 93 the number of HPAI outbreaks in the country so far this year.</p>
-                        </Paper>
-
-                        </Grid>
-                        <Grid key={3} item>
-                        <Paper elevation={0} className={styles.paper}>
-                            <p className={styles.text}>Foot and Mouth Disease</p>
-                            <p className={styles.textInner}>Families and early childhood education and care (ECEC) providers in Far North Queensland have been warned to remain on the alert for symptoms of Hand, Foot and Mouth disease (HFMD) in young children after dozens of cases were identified by public health officials in recent months.While it is common for health providers to see a small number of cases during the hotter parts of the year, this year has been especially severe.To prevent the disease spreading, ensure you practice good hand hygiene; clean and disinfect frequently touched surfaces and soiled items, including toys; avoid sharing cups, eating utensils, towels, and clothing; and teach children about cough and sneeze etiquette. </p>
-                        </Paper>
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </Grid>
-
-
 
         </div>
     )
@@ -117,8 +134,33 @@ var useStyles = makeStyles({
         width: '33.33%'
     },
     paper: {
-        height: '30vh',
+        height: '40vh',
         width: '32vw',
+        margin: '0.5vw',
+        
     },
+    img: {
+        height: '25vh',
+        textAlign: 'center',
+        display: 'block',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 'auto',
+    },
+    date: {
+        position: 'absolute',
+        alignItems: 'right',
+        paddingLeft: '24%',
+        paddingTop: '0.45%',
+        fontStyle: 'italic',
+    },
+    dateFirst: {
+        position: 'absolute',
+        alignItems: 'right',
+        paddingLeft: '26%',
+        paddingTop: '0.45%',
+        fontStyle: 'italic',
+    }
+
     
 });
